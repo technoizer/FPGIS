868621e1d2b3bd4d2f71912504fd56bca74b1510
Dinar Winia Mahandhira <dinar.mahandhira@gmail.com>
2015-06-03 14:19:50 +0700

    asdsadsadasd

--- application/views/help.php
+++ application/views/help.php
@@ -112,71 +112,7 @@
         </div><!-- /.navbar-collapse -->
       </div><!-- /.container-fluid -->
     </nav>
-    <div>
-            <style type="text/css">
-            /* make sidebar nav vertical */ 
-            @media (min-width: 768px) {
-              .left-nav .leftnavbar .leftnavbar-collapse {
-                padding: 0;
-                max-height: none;
-              }
-              .left-nav .leftnavbar ul {
-                float: none;
-              }
-              .left-nav .leftnavbar ul:not {
-                display: block;
-              }
-              .left-nav .leftnavbar li {
-                float: none;
-                display: block;
-              }
-              .left-nav .leftnavbar li a {
-                padding-top: 12px;
-                padding-bottom: 12px;
-              }
-            }
-            </style>
-            <div class="row">
-              <div class="col-sm-3">
-                <div class="left-nav">
-                  <div class="leftnavbar leftnavbar-default" role="navigation">
-                    <div class="leftnavbar-header">
-                      <button type="button" class="leftnavbar-toggle" data-toggle="collapse" data-target=".left-leftnavbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                      </button>
-                      <span class="visible-xs leftnavbar-brand">Sidebar menu</span>
-                    </div>
-                    <div class="leftnavbar-collapse collapse left-leftnavbar-collapse">
-                      <ul class="nav leftnavbar-nav">
-                        <li class="active"><a href="#">Menu Item 1</a></li>
-                        <li><a href="#">Menu Item 2</a></li>
-                        <li class="dropdown">
-                          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                          <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li class="divider"></li>
-                            <li class="dropdown-header">Nav header</li>
-                            <li><a href="#">Separated link</a></li>
-                            <li><a href="#">One more separated link</a></li>
-                          </ul>
-                        </li>
-                        <li><a href="#">Menu Item 4</a></li>
-                        <li><a href="#">Reviews <span class="badge">1,118</span></a></li>
-                      </ul>
-                    </div><!--/.nav-collapse -->
-                  </div>
-                </div>
-              </div>
-              <div class="col-sm-9">
-                Main content goes here
-              </div>
-            </div>
-    </div>
+    
     <input type="hidden" value="<?php echo $_SESSION["username"]; ?>" id="user">
 
     <div id="mapContainer"></div>
